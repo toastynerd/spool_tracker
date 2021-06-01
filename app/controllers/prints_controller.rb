b833b2c5e4e275235fcd6be1ac571d1dd56b3587
toastynerd <tylermorgan86@gmail.com>
2021-05-31 23:50:52 -0700

    add print crud

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,4 +1,50 @@
 class PrintsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_spools
+
   def index
+    @prints = current_user.prints
+  end
+
+  def new
+    @print = current_user.prints.build
+  end
+
+  def create
+    @print = current_user.prints.build(print_params)
+    if @print.save
+      redirect_to prints_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @print = Print.find(params[:id])
+  end
+
+  def update
+    @print = Print.find(params[:id])
+    if @print.update(print_params)
+      redirect_to prints_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @print = Print.find(params[:id])
+    @print.destroy
+
+    redirect_to prints_path
+  end
+
+  private
+  def print_params
+    params.require(:print).permit(:spool_id, :settings, :file, :success, :description, :print_name, :print_notes)
+  end
+
+  def get_spools
+    @spools = current_user.spools
   end
 end
--- test/controllers/prints_controller_test.rb
+++ test/controllers/prints_controller_test.rb
@@ -3,10 +3,54 @@ require "test_helper"
 class PrintsControllerTest < ActionDispatch::IntegrationTest
   setup do
     sign_in users(:test_user)
+    @spool = spools(:test_spool)
+    @print_params = {
+      spool_id: @spool.id,
+      settings: "some test settings",
+      file: "test.example.com",
+      success: true,
+      description: "a test print",
+      print_name: "test_print"
+    }
   end
 
   test "should have an index" do
     get prints_path
     assert_response :success
+  end
+
+  test "should have a new page" do
+    get new_print_path
+    assert_response :success
+  end
+
+  test "should be able to create a new print" do
+    post prints_path, params: {print: @print_params}
+    assert_response :redirect
+  end
+
+  test "should stay on same page if it doesn't pass validations" do
+    post prints_path, params: {print: {settings: ""}}
+    assert_response :success
+  end
+
+  test "should have an edit route" do
+    get edit_print_path(prints(:print_one))
+    assert_response :success
+  end
+
+  test "should update with a patch" do
+    patch print_path(prints(:print_one), {print: @print_params})
+    assert_response :redirect
+  end
+
+  test "should update with a put" do
+    put print_path(prints(:print_one), {print: @print_params})
+    assert_response :redirect
+  end
+
+  test "should be able to destroy a print" do
+    delete print_path(prints(:print_two))
+    assert_response :redirect
   end
 end
